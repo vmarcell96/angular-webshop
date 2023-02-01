4917d6a10b2cb85352d954958e6914a7a58d9034
vmarcell96 <vajdam96@gmail.com>
2023-02-01 23:35:29 +0100

    FEAT: created product detail guard, getproduct function

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/types/product';
 
 @Component({
@@ -8,15 +9,26 @@ import { Product } from 'src/app/types/product';
 })
 export class ProductDetailComponent {
   pageTitle: string = 'Product Detail';
+  errorMessage = '';
 
   // Chose undefined instead of bang, because getProduct is async
   product: Product | undefined;
 
   constructor(private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router, private productService: ProductService) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
   }
 
   onBack(): void {
