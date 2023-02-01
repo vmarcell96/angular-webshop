efbbec3b291a78f5d7633691eeda8f939153f6b4
vmarcell96 <vajdam96@gmail.com>
2023-02-01 23:04:11 +0100

    FEAT: created welcome component for homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule, 
