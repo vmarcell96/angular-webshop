03aed50be5eb391a06bff9d0766e279e4233a8cb
vmarcell96 <vajdam96@gmail.com>
2023-02-03 11:59:03 +0100

    REFACTOR: added backend project, changed directory structure

---
+++ angular-webshop/src/app/types/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    "productId": number;
+    "productName": string;
+    "productCode": string;
+    "releaseDate": string;
+    "price": number;
+    "description": string;
+    "starRating": number;
+    "imageUrl": string;
+};
