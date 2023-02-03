dd9554a8dca9fc33a9c7b246a6ab8b85dde25ae3
vmarcell96 <vajdam96@gmail.com>
2023-02-03 13:35:41 +0100

    FEAT: created basic entities

--- AngularWebshop.BusinessLayer/Customer.cs
+++ AngularWebshop.BusinessLayer/Customer.cs
@@ -2,6 +2,16 @@
 {
     public class Customer
     {
+        public Customer()
+        {
+
+        }
+
+        public Customer(int customerId)
+        {
+            CustomerId= customerId;
+        }
+
         public int CustomerId { get; private set; }
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
@@ -18,13 +28,65 @@
                 _lastName = value;
             }
         }
+
+        
+
         public string FullName
         {
             get
             {
-                return LastName + "," + FirstName;
+                string fullName = LastName;
+                //There is a firstname
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    //there is a lastname
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
             }
         }
+        /// <summary>
+        /// Validates the customer data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
 
+            if (string.IsNullOrWhiteSpace(LastName)) isValid= false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid= false;
+
+            return isValid;
+        }
+
+        /// <summary>
+        /// Retrieve one customer.
+        /// </summary>
+        public Customer Retrieve(int customerId) 
+        {
+            
+            return new Customer();
+        }
+
+        /// <summary>
+        /// Retrieve all customers.
+        /// </summary>
+        public List<Customer> Retrieve()
+        {
+
+            return new List<Customer>();
+        }
+        /// <summary>
+        /// Saves the current customer.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            return true;
+        }
     }
 }
