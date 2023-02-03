fb2081ac016bb3beb13cce01a5274415f44b3f0d
vmarcell96 <vajdam96@gmail.com>
2023-02-03 12:00:13 +0100

    FEAT: created Customer class

---
+++ AngularWebshop.BusinessLayer/Customer.cs
@@ -0,0 +1,30 @@
+﻿namespace AngularWebshop.BusinessLayer
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+
+        private string _lastName;
+        public string LastName
+        { 
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
+
+    }
+}
