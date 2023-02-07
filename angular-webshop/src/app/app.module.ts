4fe7ac8aff910af16115d7353150500f93d9c7a4
vmarcell96 <vajdam96@gmail.com>
2023-02-07 13:36:20 +0100

    REFACTOR: outsourced product and shared modules

--- angular-webshop/src/app/app.module.ts
+++ angular-webshop/src/app/app.module.ts
@@ -3,30 +3,28 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
+import { ProductModule } from './products/product.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
-    BrowserModule, 
-    AppRoutingModule,
+    BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ProductModule,
+    RouterModule.forRoot([
+      //More specific routes should always be before less specific
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ])
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
