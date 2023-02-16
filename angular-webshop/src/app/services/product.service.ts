b5167bbe26a98132a16aeaf042953613580a1a53
vmarcell96 <vajdam96@gmail.com>
2023-02-16 13:05:46 +0100

    FEAT: implemented create,update,delete operations in product service class, also it uses the in memory web api

--- angular-webshop/src/app/services/product.service.ts
+++ angular-webshop/src/app/services/product.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, catchError, tap, throwError, map } from "rxjs";
+import { Observable, catchError, tap, throwError, map, of } from "rxjs";
 import { Product } from "../types/product";
 
 @Injectable({
@@ -11,28 +11,69 @@ export class ProductService {
 
     // api folder added in angular.json file in the assets array
     // it can be used as a mock api
-    private productUrl = 'api/products/products.json';
+    private productsUrl = 'api/products';
 
     constructor(private http: HttpClient) {
 
     }
 
     getProducts(): Observable<Product[]> {
-        return this.http.get<Product[]>(this.productUrl).pipe(
+        return this.http.get<Product[]>(this.productsUrl).pipe(
             //with tap we can access the emitted item without modifying it
+            tap(product => console.log(product)),
             catchError(this.handleError)
         );
     }
 
     // Get one product
-    // Since we are working with a json file, we can only retrieve all products
-    // So retrieve all products and then find the one we want using 'map'
-    getProduct(id: number): Observable<Product | undefined> {
-        return this.getProducts()
+    getProduct(id: number): Observable<Product> {
+        // if id is 0 the user wants to add a new product, so we have to initialize one to return
+        // maybe its defined on the back end server so this block is not necessary
+        if (id === 0) {
+            //of() converts an object into an observable
+            return of(this.initializeProduct());
+        }
+        const url = `${this.productsUrl}/${id}`;
+        return this.http.get<Product>(url)
             .pipe(
-                map((products: Product[]) => products.find(p => p.productId === id))
+                //for debugging
+                tap(data => console.log('getProduct: ' + JSON.stringify(data))),
+                catchError(this.handleError)
             );
     }
+
+    updateProduct(product: Product): Observable<Product> {
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        const url = `${this.productsUrl}/${product.id}`;
+        return this.http.put<Product>(url, product, { headers })
+            .pipe(
+                tap(() => console.log('updateProduct: ' + product.id)),
+                // Return the product on an update
+                // we dont get a returned product automatically
+                map(() => product),
+                catchError(this.handleError)
+            );
+    }
+
+    createProduct(product: Product): Observable<Product> {
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        product.id = null;
+        return this.http.post<Product>(this.productsUrl, product, { headers })
+            .pipe(
+                tap(data => console.log('createProduct: ' + JSON.stringify(data))),
+                catchError(this.handleError)
+            );
+    }
+
+    deleteProduct(id: number): Observable<{}> {
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        const url = `${this.productsUrl}/${id}`;
+        return this.http.delete<Product>(url, { headers })
+          .pipe(
+            tap(data => console.log('deleteProduct: ' + id)),
+            catchError(this.handleError)
+          );
+      }
 
 
 
@@ -50,5 +91,20 @@ export class ProductService {
         }
         console.error(errorMessage);
         return throwError(() => errorMessage);
+    }
+
+    private initializeProduct(): Product {
+        // Return an initialized object
+        return {
+            "id": 0,
+            "productName": "",
+            "productCode": "",
+            "tags": [''],
+            "releaseDate": "",
+            "price": 0,
+            "description": "",
+            "starRating": 0,
+            "imageUrl": ""
+        };
     }
 }
