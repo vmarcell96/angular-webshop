465e7a1419c1b59007a24f66a205aecea04d0d0b
vmarcell96 <vajdam96@gmail.com>
2023-02-16 13:02:28 +0100

    FEAT: there is an edit button on the productlist product rows

--- angular-webshop/src/app/products/product-detail/product-detail.component.ts
+++ angular-webshop/src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/types/product';
@@ -7,7 +7,7 @@ import { Product } from 'src/app/types/product';
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   errorMessage = '';
 
